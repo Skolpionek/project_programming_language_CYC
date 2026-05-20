b4135e841d710c30091f14a1628a2a2368c6444e
Skolpionek <przybylao13@gmail.com>
2026-05-20 21:18:14 +0200

    Initial release of cyc interpreter
    - Added core language environment, parser, and basic mathematical operations.
    - Implemented primitive data types and AST-based evaluation.

---
+++ environment.js
@@ -0,0 +1,287 @@
+//----------------------
+//ERRORY
+//----------------------
+
+class SyntaxError extends Error{}
+class TypeMismatchError extends Error{}
+class DivisionByZero extends Error{}
+
+//----------------------
+//FUNKCJE
+//----------------------
+
+export const FUNCTIONS = Object.create(null);
+
+
+FUNCTIONS.print = (args) => {
+   
+   const formatOutput = (arg) => {
+      if (arg && arg.type === "list") {
+         let inner = arg.value.map(v => formatOutput(v)).join(" ");
+         return `(${inner})`;
+      }
+      return ((arg == 67) ? "SIX SEVEN!!!! SIX SEVEN!!! SIX SEVEN!!" : arg);
+   };
+   args = args.map(arg => formatOutput(arg));
+   
+   let output = args.join("");
+   console.log(output);
+   return output; 
+};
+
+//----------------------
+//STRUKTURY DANYCH
+//----------------------
+
+
+FUNCTIONS.list = (args) => {
+   let obj = Object.create(null);
+   obj.type = "list";
+   obj.value = args; 
+   
+   obj.add = (newArgs) => {
+      obj.value.push(...newArgs);
+      return obj.value;  
+   };
+   
+   obj.del = (amount=1) => {
+      for(let i=0;i<amount;i++)
+         obj.value.pop(); 
+      return obj.value;
+   };
+   obj.get = (index) => {
+      if (index >= obj.len) throw new SyntaxError("Index wykracza poza granice listy");
+      return obj.value[index];
+   }
+   obj.set = (value, index) => {
+      if (index >= obj.len) throw new SyntaxError("Index wykracza poza granice listy");
+      obj.value[index] = value;
+      return obj.value[index];
+   }
+
+   Object.defineProperty(obj, 'len', {
+      get: function() {
+         return this.value.length;
+      }
+   });
+   
+   return obj;
+}
+
+//operatory artytmetyczne
+FUNCTIONS["+"] = (args) => args.slice(1).reduce((acc, curr) => {
+   // 1.czy ten sam typ danych
+   if (typeof acc !== typeof curr) {
+      console.log(acc)
+      throw new TypeMismatchError(`Operacja na złych typach: próba dodania ${typeof curr} do ${typeof acc}`);
+   }
+   
+   // dozwolone typy (tylko liczba lub string)
+   if (typeof acc !== "number" && typeof acc !== "string") {
+      throw new TypeMismatchError(`Błędny typ: operator dodawania nie obsługuje typu ${typeof acc}`);
+   }
+   
+   return acc + curr;
+}, args[0]);
+
+FUNCTIONS["-"] = (args) => args.reduce((acc, curr) => {
+   if(typeof acc !== "number" && typeof curr !== "number") throw new TypeMismatchError("Operacja na złych typach");
+   return acc - curr;
+});
+FUNCTIONS["*"] = (args) => args.reduce((acc, curr) => {
+   if(typeof acc !== "number" && typeof curr !== "number") throw new TypeMismatchError("Operacja na złych typach");
+   return acc * curr
+}
+, 1);
+FUNCTIONS["^"] = (args) => args.reduce((acc, curr) => {
+   if(typeof acc !== "number" && typeof curr !== "number") throw new TypeMismatchError("Operacja na złych typach");
+   return Math.pow(acc, curr)
+});
+FUNCTIONS["/"] = (args) => {
+   return args.reduce((acc, curr) => {
+      if(typeof acc !== "number" && typeof curr !== "number") throw new TypeMismatchError("Operacja na złych typach");
+      if (curr === 0) throw new DivisionByZero("Nie można dzielić przez zero!");
+      return acc / curr;
+   });
+};
+
+//Operatory porówniania
+let comparisonOperators = ["==","!=","<","<=",">",">="];
+comparisonOperators.forEach(operator => {
+   let functionBody = `
+      for (let i = 0; i < args.length - 1; i++) {
+         if (!(args[i] ${operator} args[i + 1])) {
+            return false; 
+         }
+      }
+      return true;
+   `;
+   FUNCTIONS[operator] = Function("args", functionBody);
+});
+
+//Operatory logiczne
+FUNCTIONS.and = (args) => args.every(arg => Boolean(arg));
+FUNCTIONS.or = (args) => args.some(arg => Boolean(arg));
+FUNCTIONS.nand = (args) => !(FUNCTIONS.and(args));
+FUNCTIONS.nor = (args) => !(FUNCTIONS.or(args));
+FUNCTIONS.not = (args) => {
+   if(args.length !== 1) throw new SyntaxError("not przyjmuje tylko jeden argument: not(wyrazenie)");
+   return !args[0];
+};
+
+//----------------------
+//INSTRUKCJE SPECJALNE
+//----------------------
+
+export const SPECIAL_FORMS = Object.create(null);
+
+SPECIAL_FORMS.do = (args, env, evaluate) => {
+   let value = false;
+   for (let arg of args) {
+      value = evaluate(arg, env);
+   }
+   return value;
+}
+SPECIAL_FORMS.define = (args, env, evaluate) => {
+   if (args.length !== 2 || args[0].type !== "word") {
+      throw new SyntaxError("Niepoprawne przypisanie wartosci. Poprawne: =(nazwa, wartość)");
+   }
+   let value = evaluate(args[1], env);
+   if (!args[0].valueType) {
+      if(typeof value === "object"){
+         args[0].valueType = value.type;
+
+      } else {
+         args[0].valueType = typeof value
+      }
+      
+   } 
+   if (typeof value !== "object" && args[0].valueType !== typeof value){
+      throw new SyntaxError(`Niezgodność typu dla zmiennej '${args[0].name}' typu '${args[0].valueType}' z wartością: ${value} typu '${typeof value}'`)
+   }
+   
+   env[args[0].name] = value;
+   return value;
+};
+SPECIAL_FORMS.set = (args, env, evaluate) => {
+   if (args.length !== 2 || args[0].type !== "word") {
+      throw new SyntaxError("Niepoprawne przypisanie wartosci. Poprawne: =(nazwa, wartość)");
+   }
+   if (args[0].valueType) {
+      throw new SyntaxError(`Nie możesz podawać typu przy aktualizacji zmiennej! Usuń typowanie przy zmiennej '${args[0].name}'.`);
+   }
+   let varName = args[0].name;
+   let targetEnv = env;
+   while (targetEnv && !Object.prototype.hasOwnProperty.call(targetEnv, varName)) {
+      targetEnv = Object.getPrototypeOf(targetEnv);
+   }
+
+   if (!targetEnv) throw new ReferenceError(`Zmienna '${varName}' nie jest zdefiniowana.`);
+   let value = evaluate(args[1], env);
+   let oldValue = targetEnv[varName];
+
+   if (typeof value !== "object" && typeof oldValue !== typeof value) {
+      throw new SyntaxError(`Niezgodność typu dla zmiennej '${varName}' typu '${typeof oldValue}' z wartością: ${value} typu '${typeof value}'`);
+   } else if (typeof value === "object" && oldValue.type !== value.type) {
+      throw new SyntaxError(`Niezgodność typu dla obiektu '${varName}'`);
+   }
+
+   targetEnv[varName] = value;
+   return value;
+};
+SPECIAL_FORMS["="] = (args, env, evaluate) => {
+   if (args[0].name in env) SPECIAL_FORMS.set(args,env,evaluate);
+   else SPECIAL_FORMS.define(args,env,evaluate);
+}
+SPECIAL_FORMS["++"] = (args, env, evaluate,sex=1) => {
+   if(!args.length)return 1*sex;
+   args.forEach(arg => {
+      if(typeof env[arg.name] !== "number"){
+         throw new TypeMismatchError(`Zmienna ${arg.name} nie jest typu 'number', nie można jej inkrementować`);
+      }
+      SPECIAL_FORMS.define([{...arg, valueType: 'number'}, Number(evaluate(arg,env)+sex)],env,evaluate)
+   });
+   let values = args.map(arg => env[arg.name])
+   if(values.length === 1)
+      return values[0];
+   return values;
+};
+SPECIAL_FORMS["--"] = (args, env, evaluate) => SPECIAL_FORMS["++"](args, env, evaluate,-1);
+
+SPECIAL_FORMS.if = (args, env, evaluate) => {
+   let condition = evaluate(args[0], env);
+   if(args.length === 3){
+      if (condition !== false && condition !== 0 && condition !== "") {
+      return evaluate(args[1], env);
+      } else {
+         return evaluate(args[2], env);
+      }
+   }
+   else if(args.length === 2){
+      if (condition !== false && condition !== 0 && condition !== "") {
+         return evaluate(args[1], env);
+      }
+   }
+   else {
+      throw new SyntaxError("Niepoprawne użycie if. Poprawne: if(warunek, prawda, fałsz)");
+   }  
+};
+
+SPECIAL_FORMS.while = (args, env, evaluate) => {
+   if (args.length !== 2) {
+      throw new SyntaxError("Niepoprawne użycie while. Poprawne: while(warunek, instrukcja)");
+   }
+   while (evaluate(args[0], env) !== false) {
+      evaluate(args[1], env);
+      
+   }
+   
+   return false;
+}
+SPECIAL_FORMS.for = (args, env, evaluate) => {
+   if (args.length !== 4) {
+      throw new SyntaxError("Niepoprawne użycie for. Poprawne: for(inicjalizacja; warunek; krok; ciało)");
+   }
+   let localEnv = Object.create(env);
+
+   evaluate(args[0], localEnv);
+
+   while (evaluate(args[1], localEnv) !== false) {
+      evaluate(args[3], localEnv);
+      evaluate(args[2], localEnv);
+   }
+
+   return false;
+};
+SPECIAL_FORMS.func = (args, env, evaluate) => {
+   if(!args.length) {
+      throw new SyntaxError("Funkcja poprzebuje ciała: func(...args,body)");
+   }
+   let body = args[args.length-1];
+   let params = args.slice(0, args.length-1).map(expr => {
+      if (expr.type != "word") {
+         throw new SyntaxError("Nazwy parametrów muszą być zmiennymi");
+      }
+      return expr.name;
+   });
+   return function(args) {
+      if(args.length != params.length) {
+         throw new TypeError("Zła liczba argumentów")
+      }
+      let localEnv = Object.create(env);
+      for (let i=0;i<args.length;i++){
+         localEnv[params[i]] = args[i];
+      }
+      return evaluate(body, localEnv);
+   }
+}
+
+//----------------------
+// Zmienne Wbudowane
+//----------------------
+
+export const VARIABLES = Object.create(null);
+
+VARIABLES.true = true;
+VARIABLES.false = false;
+VARIABLES.penis = "<===========3";
---
+++ main.js
@@ -0,0 +1,172 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import { FUNCTIONS, SPECIAL_FORMS, VARIABLES } from './environment.js';
+
+class SyntaxError extends Error{}
+
+
+function skipSpace(string) {
+   let skippable = string.match(/^(\s|#.*)*/);
+   return string.slice(skippable[0].length);
+}
+
+function parseExpression(program) {
+   program = skipSpace(program);
+   let match, expr;
+   
+   if (match = /^"([^"]*)"/.exec(program)) {
+      expr = {type: "value", value: match[1]};
+   } else if (match = /^\d+\b/.exec(program)) {
+      expr = {type: "value", value: Number(match[0])};
+      
+   } else if (match = /^([^\s(),#":]+)(?::([a-zA-Z0-9_]+))?/.exec(program)) {
+      
+      let fullName = match[1]; 
+      
+      if (fullName.includes(".")) {
+         let parts = fullName.split(".");
+         expr = {
+            type: "property", 
+            object: parts[0],
+            property: parts[1] 
+         };
+      } else {
+         expr = {type: "word", name: fullName};
+      }
+      
+      if (match[2]) {
+         switch(match[2]){
+            case "bool": expr.valueType = "boolean"; break;
+            case "str": expr.valueType = "string"; break;
+            case "num": expr.valueType = "number"; break;
+            case "list": expr.valueType = "list"; break;
+            case "func": expr.valueType = "function"; break;
+            case "any": expr.valueType = "anything"; break;
+         }
+      }
+      
+   } else {
+      throw new SyntaxError("Niepoprawna składnia: " + program);
+   }
+   return parseApply(expr, program.slice(match[0].length));
+}
+
+function parseApply(expr, program) {
+   program = skipSpace(program);
+   if (program[0] != "(") {
+      return {expr: expr, rest: program};
+   }
+   program = skipSpace(program.slice(1));
+   expr = {type: "apply", operator: expr, args: []};
+   while (program[0] != ")") {
+      let arg = parseExpression(program);
+      expr.args.push(arg.expr);
+      program = skipSpace(arg.rest);
+      if (program[0] == "," || program[0] == ";") {
+         program = skipSpace(program.slice(1));
+      } else if (program[0] != ")") {
+         throw new SyntaxError("Oczekiwano ',' ';' lub ')'");
+      }
+   }
+   return parseApply(expr, program.slice(1));
+}
+
+function parse(program) {
+   let {expr, rest} = parseExpression(program);
+   if (skipSpace(rest).length > 0) {
+      throw new SyntaxError("Niepoprawna składnia po programie");
+   }
+   return expr;
+}
+
+function evaluate(parsedExpression, env) {
+   if (parsedExpression.type === "value") return parsedExpression.value;
+   
+   if (parsedExpression.type === "word") {
+      if (parsedExpression.name in VARIABLES) {
+         return VARIABLES[parsedExpression.name];
+      }
+      if (parsedExpression.name in env) {
+         return env[parsedExpression.name];
+      } else {
+         throw new ReferenceError(`Niezdefiniowana zmienna: ${parsedExpression.name}`);
+      }
+   }
+
+   if (parsedExpression.type === "property") {
+      let obj = env[parsedExpression.object];
+      if (!obj) throw new ReferenceError(`Niezdefiniowany obiekt: ${parsedExpression.object}`);
+      
+      return obj[parsedExpression.property];
+   }
+   
+   if (parsedExpression.type === 'apply') {
+      if (parsedExpression.operator.type === "property") {
+         let objName = parsedExpression.operator.object;
+         let methodName = parsedExpression.operator.property;
+         
+         let obj = env[objName];
+         if (!obj) throw new ReferenceError(`Niezdefiniowany obiekt: ${objName}`);
+         if (typeof obj[methodName] !== "function") throw new TypeError(`${methodName} nie jest metodą!`);
+         
+         let evaluatedArgs = parsedExpression.args.map(arg => evaluate(arg, env));
+         return obj[methodName](evaluatedArgs);
+      }
+
+      let currentOperator = parsedExpression.operator.name;
+      
+      if (currentOperator in SPECIAL_FORMS) {
+         return SPECIAL_FORMS[currentOperator](parsedExpression.args, env, evaluate);
+      } 
+      
+      if (currentOperator in FUNCTIONS) {
+         let evaluatedArgs = parsedExpression.args.map(arg => evaluate(arg, env));
+         let result = FUNCTIONS[currentOperator](evaluatedArgs);
+         return result;
+      }
+      if (currentOperator in env) {
+         let funcObj = env[currentOperator];
+         if (typeof funcObj !== "function") {
+             throw new TypeError(`'${currentOperator}' nie jest funkcją.`);
+         }
+         let evaluatedArgs = parsedExpression.args.map(arg => evaluate(arg, env));
+         return funcObj(evaluatedArgs);
+      }
+      throw new SyntaxError(`Nieznana funkcja lub operator: ${currentOperator}`);
+   }
+   
+   return parsedExpression;
+}
+
+function interprete(expression){
+   let parsed = parse(`do(${expression})`);
+   let globalEnv = Object.create(null);
+   return evaluate(parsed, globalEnv)
+}
+
+function runCLI() {
+   let args = process.argv.slice(2);
+   
+   if (args.length === 0) {
+      console.log("Użycie: cyc <nazwa_pliku.cyc>");
+      process.exit(1);
+   }
+   
+   let filename = args[0];
+   
+   try {
+      let fileContent = fs.readFileSync(filename, 'utf8');
+      interprete(fileContent);
+   } catch (error) {
+      console.error(`\nBłąd interpretera:\n${error.message}`);
+      process.exit(1);
+   }
+}
+
+runCLI();
+//npm link
+
+
+
+
